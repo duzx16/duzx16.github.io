8d0283540d7f2851548b9d1ff9c0930104b0b17e
duzx <duzx16@mails.tsinghua.edu.cn>
2020-08-07 23:14:12 +0800

    update on 20/08/07

--- js/mathjax-config.js
+++ js/mathjax-config.js
@@ -1,21 +1,16 @@
+// MathJax Configuration
+//
+// v2 to v3 upgrade notes:
+// - The CommonHTML.linebreaks option is not yet implemented (but may be in a future release)
+// - The TeX.noUndefined.attributes option is not yet implemented (but may be in a future release)
 window.MathJax = {
-  CommonHTML: {linebreaks: {automatic: true}},
-  tex2jax: {
+  tex: {
     inlineMath: [['$', '$'], ['\\(', '\\)']],
     displayMath: [['$$', '$$'], ['\\[', '\\]']],
     processEscapes: false,
-    skipTags: ['script', 'noscript', 'style', 'textarea', 'pre']  // Remove `code` (required for inline math).
+    packages: {'[+]': ['noerrors']}
   },
-  TeX: {noUndefined: {attributes: {mathcolor: 'red', mathbackground: '#FFEEEE', mathsize: '90%'}}},
-  messageStyle: 'none'
-};
-
-MathJax.Hub.Queue(function() {
-  // Fix inline math wrapped in <code> tags after MathJax finishes running.
-  // This is a workaround to overcome a shortcoming of Blackfriday Markdown.
-  // See discussion in Hugo Docs.
-  let all = MathJax.Hub.getAllJax(), i;
-  for (i = 0; i < all.length; i += 1) {
-    all[i].SourceElement().parentNode.className += ' has-jax';
+  loader: {
+    load: ['[tex]/noerrors']
   }
-});
+};
